7a10f5daffcfb7518a7ed8f71c6b0a558381b621
Lubosz Sarnecki <lubosz@gmail.com>
2023-12-31 16:28:47 +0100

    Add class boilerplate.

---
+++ src/slic3r/GUI/TAAManager.cpp
@@ -0,0 +1,19 @@
+/*
+ * PrusaSlicer
+ * Copyright 2023 Lubosz Sarnecki <lubosz@gmail.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#include "TAAManager.hpp"
+
+namespace Slic3r {
+namespace GUI {
+
+TAAManager::TAAManager()
+{
+    printf("Hellow TAAManager!");
+}
+
+
+} // namespace GUI
+} // namespace Slic3r
