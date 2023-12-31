c28a3871dd66166529996ea44259049efd68b713
Lubosz Sarnecki <lubosz@gmail.com>
2023-12-31 16:30:48 +0100

    fixup: save header.

---
+++ src/slic3r/GUI/TAAManager.hpp
@@ -0,0 +1,20 @@
+/*
+ * PrusaSlicer
+ * Copyright 2023 Lubosz Sarnecki <lubosz@gmail.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#pragma once
+
+namespace Slic3r {
+namespace GUI {
+
+class TAAManager
+{
+  public:
+    TAAManager();
+    ~TAAManager() { }
+};
+
+} // namespace GUI
+} // namespace Slic3r
